59c5a88079fe82977ce4a97c462e6490fe4ec613
ATTHIRAJU RAVITEJA <athirajuteja5@gmail.com>
2026-08-18 14:50:11 +0530

    Change base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/bottle-spinner/',
+  base: '/Spin-The-Bottle/',
   plugins: [react()],
   server: {
     host: true
